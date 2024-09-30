410213413546ae319b1c1e4fa9d172daba9419a1
SimonH <professional.simonhossain@gmail.com>
2024-09-30 18:12:59 +0100

    debugging

---
+++ cmake-build-debug/CMakeFiles/C__.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C__.dir/main.cpp.o"
+  "CMakeFiles/C__.dir/main.cpp.o.d"
+  "CMakeFiles/C__.dir/src/Battle.cpp.o"
+  "CMakeFiles/C__.dir/src/Battle.cpp.o.d"
+  "CMakeFiles/C__.dir/src/Pokeball.cpp.o"
+  "CMakeFiles/C__.dir/src/Pokeball.cpp.o.d"
+  "CMakeFiles/C__.dir/src/Pokemon.cpp.o"
+  "CMakeFiles/C__.dir/src/Pokemon.cpp.o.d"
+  "CMakeFiles/C__.dir/src/Trainer.cpp.o"
+  "CMakeFiles/C__.dir/src/Trainer.cpp.o.d"
+  "C__"
+  "C__.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/C__.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
