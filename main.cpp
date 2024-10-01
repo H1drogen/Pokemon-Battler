07fdd70fbb4a08c27c6fa0857f55911e9d5cd5ff
SimonH <professional.simonhossain@gmail.com>
2024-10-01 15:47:15 +0100

    refactoring pokemon object

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#include "src/Pokemon.h"
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+
+    Pokemon pikachu("Pikachu", 100, 50, "Thunderbolt");
+    std::cout << "Created a Pokemon: " << pikachu.name << std::endl;
 
     return 0;
 }
--- src/Pokemon.h
+++ src/Pokemon.h
@@ -1,6 +1,19 @@
-
+#include <iostream>
 #ifndef C___POKEMON_H
 #define C___POKEMON_H
 
+class Pokemon {
+public:
+    std::string name;
+    int hit_points;
+    int attack_damage;
+    std::string move;
+
+    Pokemon(std::string name, int damage);
+    void take_damage(int damage);
+    bool has_fainted();
+    // Additional methods as needed
+
+};
 
 #endif //C___POKEMON_H
