82780179538b706e1ffc029ec69552e80b2a286e
SimonH <professional.simonhossain@gmail.com>
2025-04-01 23:37:42 +0100

    public header properties

--- src/include/Battle.h
+++ src/include/Battle.h
@@ -5,5 +5,18 @@
 #ifndef C___BATTLE_H
 #define C___BATTLE_H
 
+#include <memory>
+#include "Pokemon.h"
+
+
+class Battle {
+public:
+    Battle(std::shared_ptr<Pokemon> p1, std::shared_ptr<Pokemon> p2);
+    void take_turn();
+    std::shared_ptr<Pokemon> determine_winner();
+
+
+};
+
 
 #endif //C___BATTLE_H
