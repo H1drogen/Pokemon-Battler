449736865b890eec530d4d04b16a40813c808484
SimonH <professional.simonhossain@gmail.com>
2025-06-12 23:54:45 +0100

    creating new pokemonss

--- main.cpp
+++ main.cpp
@@ -8,7 +8,17 @@ int main() {
     std::cout << "Created a Move: " << thunderbolt.name << std::endl;
 
     Pokemon pikachu("Pikachu", 100, thunderbolt, "Electric");
-    std::cout << "Created a Pokemon: " << pikachu.name << std::endl;
+    std::cout << "Created a Pokemon: " << pikachu.name <<" with HP: " << pikachu.hit_points
+    << std::endl;
+
+    pikachu.take_damage(20);
+
+    std::cout << "Pikachu took damage, remaining HP: " << pikachu.hit_points << std::endl;
+
+    Pokemon charizard("Charizard", 150, Move("Flamethrower", 90, "Fire"), "Fire");
+
+    std::cout << "Created a Pokemon: " << charizard.name <<" with HP: " << charizard.hit_points
+    << std::endl;
     
 
     return 0;
