3c0d8f549f5b9c33fd616d1c4f50edc531c2718c
SimonH <professional.simonhossain@gmail.com>
2025-06-29 23:52:06 +0100

    declaring in h files

--- src/include/Trainer.h
+++ src/include/Trainer.h
@@ -2,4 +2,15 @@
 #ifndef C___TRAINER_H
 #define C___TRAINER_H
 
+#include <iostream>
+#include <vector>
+
+class Trainer {
+public:
+    std::vector<std::shared_ptr<Pokemon>> belt;
+
+    Trainer() : belt(6, nullptr) {}
+    void throw_pokeball();
+};
+
 #endif //C___TRAINER_H
